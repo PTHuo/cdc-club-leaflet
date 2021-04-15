f4f97323d6407177d1b80b4e340d822226ab6712
PTHuo <fkaeppelin@gmail.com>
2021-04-14 22:26:17 -0700

    Correcting mistake

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,10 +41,6 @@ const IndexPage = () => {
       <Container type="content" className="text-center home-start">
         <h2>Still Getting Started?</h2>
         <p>Run the following in your terminal!</p>
-        <Snippet>
-          gatsby new [directory]
-          https://github.com/colbyfayock/gatsby-starter-leaflet
-        </Snippet>
         <p className="note">
           Note: Gatsby CLI required globally for the above command
         </p>
